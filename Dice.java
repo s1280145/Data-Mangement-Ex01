0790faee9281c9146bc71c5f62acec4320249523
s1280145 <s1280145@u-aizu.ac.jp>
2021-07-05 16:39:49 +0900

    Initial revision

---
+++ Dice.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+//import java.util.Scanner;
+
+public class Dice{
+	public static void main(String[] args) {
+		Random rand = new Random();
+		/*System.out.println("What is your name?");
+		System.out.print(">");
+		Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println("Hello, " + str +"!");
+        scan.close();*/
+		
+		System.out.println("Rolling the dice...");
+		int D1,D2,Total;
+		D1 = rand.nextInt((6)+1);
+		D2 = rand.nextInt((6)+1);
+		Total = D1+D2;
+		System.out.println("Dice 1: " + D1);
+		System.out.println("Dice 2: " + D2);
+		System.out.println("Total value: " + Total);
+		/*if(Total>7) {
+			System.out.println("You" + " won");
+		}else {
+			System.out.println("You" + " lost");
+		}*/
+	}
+}
